71fc07ff6cfa31bc9d6c2de5351fde53f9111b5d
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2022-11-20 22:01:39 -0500

    Add files via upload

---
+++ Lab1/Lab1/Crystal.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BrownianMotion
+{
+    class Crystal
+    {
+        private readonly int _n;
+        private readonly int _k;
+        private readonly double _p;
+        private readonly bool _withLock;
+        private readonly List<int> _cells;
+
+        private object _locker = new object();
+
+        public List<int> CrystalState => _cells;
+
+        public Crystal(int n, int k, double p, bool withLock)
+        {
+            _n = n;
+            _k = k;
+            _p = p;
+            _withLock = withLock;
+            _cells = new List<int>(new int[n]);
+            _cells[0] = k;
+        }
+
+        public void StartSimulation(CancellationToken stop)
+        {
+            for (int i = 0; i < _k; i++)
+            {
+                var atom = new Atom(0, _n, _p);
+                atom.AtomEvent += UpdateCrystalState;
+                var thread = new Thread(() => atom.StartMove(stop));
+                thread.Start();
+            }
+        }
+
+        private void UpdateCrystalState(object sender, AtomEventArgs e)
+        {
+            if (_withLock)
+            {
+                UpdateWithLock(e);
+            }
+            else
+            {
+                UpdateWithoutLock(e);
+            }
+        }
+
+        private void UpdateWithLock(AtomEventArgs e)
+        {
+            lock (_locker)
+            {
+                _cells[e.from] -= 1;
+                _cells[e.to] += 1;
+            }
+        }
+
+        private void UpdateWithoutLock(AtomEventArgs e)
+        {
+            _cells[e.from] -= 1;
+            _cells[e.to] += 1;
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace BrownianMotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 8;
+            var k = 50;
+            var p = 0.6;
+
+            var withLock = true;
+
+            var sleepSec = 10;
+            var checkStatEverySec = 2;
+
+            var crystal = new Crystal(n, k, p, withLock);
+
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            TimerCallback callback = new TimerCallback(WriteState);
+
+            crystal.StartSimulation(token);
+            Timer timer = new Timer(callback, crystal, TimeSpan.Zero, TimeSpan.FromSeconds(checkStatEverySec));
+            Thread.Sleep(TimeSpan.FromSeconds(sleepSec));
+
+            tokenSource.Cancel();
+
+            timer.Change(Timeout.Infinite, Timeout.Infinite);
+
+            Console.WriteLine("================= End simulation =========================");
+            WriteState(crystal);
+            Console.WriteLine("==================================== Total count =======================================");
+            Console.WriteLine(crystal.CrystalState.Sum());
+        }
+
+        public static void WriteState(object crystalObj)
+        {
+            var crystal = (Crystal)crystalObj;
+            var state = crystal.CrystalState.ToList();
+
+            var line = state.Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}");
+            Console.WriteLine(line);
+        }
+    }
+}
